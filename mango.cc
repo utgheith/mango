6bf8707296015e7f39df59077f0d041f6d4d68c9
utgheith <utgheith@users.noreply.github.com>
2025-04-17 12:39:48 -0500

    simplified

--- mango.cc
+++ mango.cc
@@ -7,14 +7,27 @@ using namespace mango;
 constexpr Nat<3> x{5};
 constexpr Nat<64> y{77};
 constexpr Nat<0> zero;
+constexpr Nat<1000> big;
+
+template <uint16_t N>
+void show(const char* msg, Nat<N> const& v) {
+  std::cout << "[" << msg << "] ";
+  std::cout << "Nat<" << N << ">{" << v << "}";
+  std::cout << std::endl;
+}
+
+#define SHOW(x) show(#x, x)
 
 int main() {
   printf("hello\n");
-  printf("%lu\n", sizeof(x));
-  std::cout << x << std::endl;
-  std::cout << zero + x << std::endl;
-  std::cout << zero + x << std::endl;
-  std::cout << x + y << " " << (x + y).WIDTH << std::endl;
+  SHOW(x);
+  SHOW(y);
+  SHOW(zero);
+  SHOW(big);
+  SHOW(Nat<500>{x});
+  // std::cout << zero + x << std::endl;
+  // std::cout << zero + x << std::endl;
+  // std::cout << x + y << " " << (x + y).WIDTH << std::endl;
 
   return 0;
 }
--- mango.h
+++ mango.h
@@ -9,123 +9,56 @@
 
 namespace mango {
 
-template <typename T>
-constexpr T max(T a, T b) {
-  return a > b ? a : b;
+constexpr uint16_t safe_sub(uint16_t a, uint16_t b) {
+  return (a < b) ? 0 : a - b;
 }
 
-template <uint16_t V>
-struct Width {
-  constexpr static uint16_t value = V;
+template <uint16_t N>
+struct Nat;
+
+template <>
+struct Nat<0> {
+  constexpr static uint64_t low = 0;
+
+  constexpr Nat() {}
+
+  template <uint16_t M>
+  explicit constexpr Nat(Nat<M> const &) {}
+
+  template <uint16_t M>
+  constexpr operator const Nat<M>() const {
+    return Nat<M>{};
+  }
+
+  constexpr const Nat<0> upper() const { return Nat<0>{}; }
 };
-
-template <typename T>
-concept Big = T::value > 64;
-
-template <typename T>
-concept Small = T::value <= 64 && T::value > 0;
-
-template <typename T>
-concept Zero = T::value == 0;
-
-template <typename T>
-  requires Big<T> || Small<T> || Zero<T>
-class NatImpl;
-
-/////////
-// Nat //
-/////////
 
 template <uint16_t N>
-using Nat = NatImpl<Width<N>>;
-
-///////////////////////////////
-// Big                       //
-//     Nat<N-64> || uint64_t //
-///////////////////////////////
-
-template <Big N>
-class NatImpl<N> {
- public:
-  constexpr static unsigned WIDTH = (N::value < 0) ? 0 : N::value;
-  constexpr static unsigned SLACK = WIDTH % 64;
-  constexpr static uint64_t HIGH = (WIDTH > 64) ? WIDTH - 64 : 0;
-
+struct Nat {
   const uint64_t low;
-  const NatImpl<Width<HIGH>> high;
+  const Nat<safe_sub(N, 64)> high;
+  constexpr static uint16_t WIDTH = N;
+  constexpr static uint16_t SLACK = (N > 64) ? 0 : 64 - N;
 
-  constexpr NatImpl(const uint64_t low = 0)
-      : low(low << SLACK >> SLACK), high{} {}
-  constexpr NatImpl(const NatImpl<Width<HIGH>> high, const uint64_t low)
-      : low(low << SLACK >> SLACK), high(high) {}
-};
+  constexpr const Nat<safe_sub(N, 64)> upper() const { return high; }
 
-//////////////////
-// Small        //
-//     uint64_t //
-//////////////////
+  constexpr Nat(const uint64_t v = 0) : low(v << SLACK >> SLACK) {}
 
-template <Small N>
-class NatImpl<N> {
- public:
-  constexpr static uint16_t WIDTH = N::value;
-  constexpr static uint16_t SLACK = 64 - WIDTH;
-
-  const uint64_t low;
-
-  constexpr NatImpl(const uint64_t l = 0) : low(l << SLACK >> SLACK) {}
-
-  template <Zero T>
-  constexpr const NatImpl<N> operator+(NatImpl<T> const &) const {
-    return *this;
-  }
-
-  template <Small T>
-  constexpr const Nat<max(T::value, N::value) + 1> operator+(
-      NatImpl<T> const &rhs) const {
-    constexpr uint16_t OUT_WIDTH = max(T::value, N::value) + 1;
-    if constexpr (OUT_WIDTH <= 64) {
-      return Nat<OUT_WIDTH>(low + rhs.low);
-    } else {
-      const uint64_t sum = low + rhs.low;
-      const Nat<OUT_WIDTH - 64> high(((sum < low) || (sum < rhs.low)) ? 1 : 0);
-      return Nat<OUT_WIDTH>(high, sum);
-    }
-  }
-};
-
-//////////
-// Zero //
-//////////
-
-template <Zero N>
-class NatImpl<N> {
- public:
-  constexpr static uint16_t WIDTH = 0;
-  constexpr NatImpl() {}
-
-  template <typename T>
-    requires Big<T> || Small<T> || Zero<T>
-  constexpr const NatImpl<T> operator+(NatImpl<T> const &rhs) const {
-    return rhs;
-  }
+  template <uint16_t M>
+  explicit constexpr Nat(Nat<M> const &src)
+      : low(src.low << SLACK >> SLACK), high(src.upper()) {}
 };
 
 ////////////
 // Output //
 ////////////
 
-template <typename T>
-  requires Big<T> || Small<T> || Zero<T>
-inline std::ostream &operator<<(std::ostream &os, const NatImpl<T> &nat) {
-  if constexpr (T::value > 64) {
+template <uint16_t N>
+inline std::ostream &operator<<(std::ostream &os, const Nat<N> &nat) {
+  if constexpr (N > 64) {
     os << nat.high << ":";
   }
-  if constexpr (T::value > 0) {
-    os << std::format("{}", nat.low);
-  } else {
-    os << "0";
-  }
+  os << nat.low;
   return os;
 }
 
