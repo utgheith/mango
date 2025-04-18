696157d161555fb4e19f792e9f13f2e49e625671
utgheith <utgheith@users.noreply.github.com>
2025-04-18 06:57:30 -0500

    inc

--- mango.cc
+++ mango.cc
@@ -7,7 +7,7 @@ using namespace mango;
 constexpr Nat<3> x{5};
 constexpr Nat<64> y{77};
 constexpr Nat<0> zero;
-constexpr Nat<1000> big;
+constexpr Nat<1000> big{x};
 
 template <uint16_t N>
 void show(const char* msg, Nat<N> const& v) {
@@ -25,6 +25,8 @@ int main() {
   SHOW(zero);
   SHOW(big);
   SHOW(Nat<500>{x});
+  SHOW(big.inc());
+
   // std::cout << zero + x << std::endl;
   // std::cout << zero + x << std::endl;
   // std::cout << x + y << " " << (x + y).WIDTH << std::endl;
--- mango.h
+++ mango.h
@@ -9,12 +9,18 @@
 
 namespace mango {
 
+typedef unsigned int uint128_t __attribute__((mode(TI)));
+
 constexpr uint16_t safe_sub(uint16_t a, uint16_t b) {
   return (a < b) ? 0 : a - b;
 }
 
 template <uint16_t N>
 struct Nat;
+
+////////////
+// Nat<0> //
+////////////
 
 template <>
 struct Nat<0> {
@@ -31,7 +37,13 @@ struct Nat<0> {
   }
 
   constexpr const Nat<0> upper() const { return Nat<0>{}; }
+
+  constexpr const Nat<1> inc() const;
 };
+
+////////////
+// Nat<N> //
+////////////
 
 template <uint16_t N>
 struct Nat {
@@ -44,10 +56,30 @@ struct Nat {
 
   constexpr Nat(const uint64_t v = 0) : low(v << SLACK >> SLACK) {}
 
+  constexpr Nat(const Nat<safe_sub(N, 64)> &high, const uint64_t low)
+      : low(low << SLACK >> SLACK), high(high) {}
+
   template <uint16_t M>
   explicit constexpr Nat(Nat<M> const &src)
       : low(src.low << SLACK >> SLACK), high(src.upper()) {}
+
+  constexpr const Nat<N + 1> inc() const {
+    const auto low1 = low + 1;
+    if constexpr (N < 64) {
+      return Nat<N + 1>{low1};
+    } else {
+      const auto low128 = uint128_t(low) + 1;
+      if (uint64_t(low128) == low1) {
+        return Nat<N + 1>{Nat<N - 63>{high}, low1};
+      } else {
+        static_assert(SLACK == 0);
+        return Nat<N + 1>{high.inc(), low1};
+      }
+    }
+  }
 };
+
+constexpr const Nat<1> Nat<0>::inc() const { return Nat<1>{1}; }
 
 ////////////
 // Output //
