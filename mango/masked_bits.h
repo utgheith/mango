2eedf47aa4fa89c20a2551b59b17e0bd96b6bd36
utgheith <utgheith@users.noreply.github.com>
2025-05-11 10:36:49 -0500

    simplified ci.yml

--- mango/masked_bits.h
+++ mango/masked_bits.h
@@ -2,21 +2,19 @@
 
 #include <cstdint>
 
-template <uint16_t N, typename MaskType, typename ValueType>
-struct MaskedBits {
+template <uint16_t N, typename MaskType, typename ValueType> struct MaskedBits {
 
-    static constexpr MaskType mask{};
-    static constexpr ValueType value{};
-    const mango::Bits<N> bits;
+  static constexpr MaskType mask{};
+  static constexpr ValueType value{};
+  const mango::Bits<N> bits;
 
-    MaskedBits(const mango::Bits<N>& bs) : bits(bs) {}
+  MaskedBits(const mango::Bits<N> &bs) : bits(bs) {}
 
-    template <uint16_t H, uint16_t L>
-    requires ((H < N) && (L <= H))
-    constexpr MaskedBits<H-L+1, decltype(mask.template extract<H,L>()), decltype(value.template extract<H,L>())> extract() const noexcept {
-        return {bits.template extract<H,L>()};
-    }
-
-    
-
+  template <uint16_t H, uint16_t L>
+    requires((H < N) && (L <= H))
+  constexpr MaskedBits<H - L + 1, decltype(mask.template extract<H, L>()),
+                       decltype(value.template extract<H, L>())>
+  extract() const noexcept {
+    return {bits.template extract<H, L>()};
+  }
 };
